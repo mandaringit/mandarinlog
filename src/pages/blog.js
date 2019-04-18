dd87c01e4bcc506469817fd2ecb99e4557086cf9
MANDARIN <compurain@naver.com>
2019-04-18 15:10:02 +0900

    gatsby-transformer-remark를 이용한 마크다운 파일 쿼리 및 사용하기

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,35 @@
 import React from "react"
 import Layout from "../components/layout"
+import { useStaticQuery, graphql } from "gatsby"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            html
+            excerpt
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout>
       <h1>블로그</h1>
-      <p>포스트는 나중에 나타날것</p>
+      <ol>
+        {data.allMarkdownRemark.edges.map(edge => (
+          <li>
+            <h2>{edge.node.frontmatter.title}</h2>
+            <p>{edge.node.frontmatter.date}</p>
+          </li>
+        ))}
+      </ol>
     </Layout>
   )
 }

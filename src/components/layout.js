cbb971250463a694e62adf68d3792e54256087cf
MANDARIN <compurain@naver.com>
2019-04-18 13:03:58 +0900

    styled-components 적용, 글로벌스타일,테마 생성

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,29 @@
 import React from "react"
 import Header from "./header"
 import Footer from "./footer"
+import styled, { ThemeProvider } from "styled-components"
+import GlobalStyles from "../styles/GlobalStyles"
+import { Theme } from "../styles/Theme"
+
+const Test = styled.div`
+  color: ${props => props.theme.basicColor};
+`
 
 const Layout = props => {
   return (
-    <div>
-      <Header />
-      {props.children}
-      <Footer />
-    </div>
+    <ThemeProvider theme={Theme}>
+      <>
+        <GlobalStyles />
+        <>
+          <div>
+            <Test>lala</Test>
+            <Header />
+            {props.children}
+            <Footer />
+          </div>
+        </>
+      </>
+    </ThemeProvider>
   )
 }
 

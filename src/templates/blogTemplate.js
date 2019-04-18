8f013819c295fedb9fc8d0e284b09173b1c13ce8
MANDARIN <compurain@naver.com>
2019-04-18 17:22:44 +0900

    템플릿에 동적으로 각 마크다운의 내용 가져오기

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,8 +1,33 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-const Blog = () => {
-  return <Layout>이건 블로그 템플릿</Layout>
+// 아직까지 useStaticQuery를 사용하여 context에 접근할 수 있는 방법이 없다.
+// 대안은 아래와 같이 export 하면, 컴포넌트에서 props로 받아서 사용 가능하다.
+
+export const query = graphql`
+  query($slug: String!) {
+    # $slug는 gatsby-node에서 createPage의 context에서 온다.
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+const Blog = props => {
+  return (
+    <Layout>
+      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+      <p>{props.data.markdownRemark.frontmatter.date}</p>
+      <div
+        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
+      />
+    </Layout>
+  )
 }
 
 export default Blog

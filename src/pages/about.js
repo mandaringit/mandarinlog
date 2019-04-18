84bfdfd8d6ce3b7b5c3f1fef93de471bfba51383
MANDARIN <compurain@naver.com>
2019-04-18 13:02:57 +0900

    레이아웃 적용

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
-import Footer from "../components/footer"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 const AboutPage = () => {
   return (
-    <div>
-      <Header />
+    <Layout>
       <h1>MANDARIN은?</h1>
       <Link to="/contact">컨택트</Link>
       <p>만다린은 남성이다.</p>
@@ -14,8 +12,7 @@ const AboutPage = () => {
       <p>만다린은 게임을 좋아한다.</p>
       <p>만다린은 노래도 좋아한다.</p>
       <p>만다린은 영화도 좋아한다.</p>
-      <Footer />
-    </div>
+    </Layout>
   )
 }
 

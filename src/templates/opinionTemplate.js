253c642435c030393af7cca86eb4140bc4d0458f
MANDARIN <compurain@naver.com>
2019-04-20 12:00:09 +0900

    오타 수정

--- src/templates/opinionTemplate.js
+++ src/templates/opinionTemplate.js
@@ -3,7 +3,7 @@ import Layout from "../components/layout"
 import { graphql } from "gatsby"
 import HelmetComponent from "../components/helmetComponent"
 import styled from "styled-components"
-import Adsense from "react-adsense"
+import AdSense from "react-adsense"
 
 // 아직까지 useStaticQuery를 사용하여 context에 접근할 수 있는 방법이 없다.
 // 대안은 아래와 같이 export 하면, 컴포넌트에서 props로 받아서 사용 가능하다.

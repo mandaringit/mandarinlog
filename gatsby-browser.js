1b800a9fc5c4f4285891977a41f6442b883ca40d
MANDARIN-PC <33564101+compurain@users.noreply.github.com>
2019-05-06 22:43:21 +0900

    서비스 워커 테스트

---
+++ gatsby-browser.js
@@ -0,0 +1,10 @@
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `이 앱이 최근 업데이트 되었습니다. ` +
+      `최근 버전을 표시하기위해 업데이트하시겠습니까?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}

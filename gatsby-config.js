58f33f248bed967bf2651a8159658e99bd91b7aa
MANDARIN-PC <33564101+compurain@users.noreply.github.com>
2019-05-06 13:11:45 +0900

    코드 컨텐츠 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
-              classPrefix: "language-js",
+              classPrefix: "language-",
               inlineCodeMarker: null,
               aliases: {},
               showLineNumbers: false,

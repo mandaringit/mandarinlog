4a66392134f7711cd1e090682139acffa3cd9349
MANDARIN <compurain@naver.com>
2019-05-08 00:32:43 +0900

    애드센스 코드 변경용

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 export const onServiceWorkerUpdateReady = () => {
   const answer = window.confirm(
-    `이 앱이 최근 업데이트 되었습니다. ` +
+    `만다린로그가 최근 업데이트 되었습니다. ` +
       `최근 버전을 표시하기위해 새로고침 하시겠습니까?`
   )
 

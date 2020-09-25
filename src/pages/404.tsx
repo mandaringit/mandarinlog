c35b9475edf7fb6c9a828fbca5aa95aa56a5c621
mandaringit <mandarin.dev@kakao.com>
2020-09-25 09:30:12 +0900

    add: 임시 404 Page

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <div>404 입니다.</div>
+    </Layout>
+  );
+};
+
+export default NotFound;
